ab90238f61d3b902fbb778494dd58edb23ed1fbe
william <williamkurniawan10@gmail.com>
2022-03-28 09:22:41 +0700

    New Update

--- project.js
+++ project.js
@@ -1,46 +1,44 @@
+let projects = [];
 
-let projects = []
-function addProject(event)
-{
-  event.preventDefault()
+function addProject(event) {
+  event.preventDefault();
 
-    let tittle = document.getElementById('input-tittle').value
-    let desc = document.getElementsByTagName('input-desc').value
-    let durasi = document.getElementById('input-durasi').value
-    let image = document.getElementById('input-image').files
+  let tittle = document.getElementById("input-tittle").value;
+  let desc = document.getElementById("input-desc").value;
+  let image = document.getElementById("input-image").files;
 
-    let project =
-    {
-        tittle : tittle,
-        desc : desc,
-        durasi : durasi,
-        image : image
-      }
+  image = URL.createObjectURL(image[0]); // Untuk menampilkan gambar
 
-    project.push(projects)
-    console.log(projects)
-    renderProject()
+  let project = {
+    tittle: tittle,
+    desc : desc,
+    image: image,
+  };
+
+  projects.push(project); // Menambahkan object project ke dalam array projects
+
+  renderProjects();
 }
 
-function renderProject()
-{
-  document.getElementById('contents').innerHTML = ' '
+function renderProjects() {
+  document.getElementById("project-view").innerHTML = "";
 
-  for (let  dataProject = 0; dataProject< projects.lenght; dataProject++)
-  {
-    document.getElementById('contents').innerHTML +=
-    `
+  for (let dataProject = 0; dataProject < projects.length; dataProject++) {
+    document.getElementById("project-view").innerHTML += `
     <div class="project-card">
           <!-- Untuk Image Tittle durasi  -->
-          <img src="assets/hotNews.jpg" id="image-card" />
-          <a href="viewProject.html" style="text-decoration: none; color: black;">
-            <h3 id="tittle-card"> ${projects[dataProject].tittle}</h3>
+          <img src="${projects[dataProject].image}" id="image-card" />
+          <a
+            href="viewProject.html"
+            style="text-decoration: none; color: black"
+          >
+            <h3 id="tittle-card">${projects[dataProject].tittle}</h3>
           </a>
-          <p class="durasi" id="durasi-card">Durasi 3 Bulan</p>  
-          
+          <p class="durasi" id="durasi-card">Durasi 3 Bulan</p>
+
           <!-- Untuk Desc -->
           <p id="desc-card">
-            ${projects[dataProject].desc}
+            ${projects[dataProject].desc.slice(0, 30)}
           </p>
 
           <!-- Icon Code -->
@@ -48,12 +46,12 @@ function renderProject()
           <i class="fa-brands fa-html5"></i>
           <i class="fa-brands fa-java"></i>
           <i class="fa-brands fa-android"></i>
-          
+
           <div class="button-card">
-              <button>Edit</button>
-              <button>Delete</button>
-          </div>      
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
         </div>
-    `
+    `;
   }
 }
